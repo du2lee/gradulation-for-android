52d362a7b149e2b549535957ab0157e916bccae5
duhui <dh151446@gmail.com>
2021-10-27 11:00:40 +0900

    load and process image

--- app/src/main/java/com/example/kickboard/DetectorActivity.java
+++ app/src/main/java/com/example/kickboard/DetectorActivity.java
@@ -20,8 +20,13 @@ import android.widget.Button;
 import android.widget.ImageView;
 
 import org.tensorflow.lite.Interpreter;
+import org.tensorflow.lite.support.common.TensorOperator;
 import org.tensorflow.lite.support.common.TensorProcessor;
+import org.tensorflow.lite.support.common.ops.NormalizeOp;
+import org.tensorflow.lite.support.image.ImageProcessor;
 import org.tensorflow.lite.support.image.TensorImage;
+import org.tensorflow.lite.support.image.ops.ResizeOp;
+import org.tensorflow.lite.support.image.ops.ResizeWithCropOrPadOp;
 import org.tensorflow.lite.support.tensorbuffer.TensorBuffer;
 
 import java.io.FileInputStream;
@@ -72,6 +77,11 @@ public class DetectorActivity extends AppCompatActivity {
         });
 
         //define the interpreter with tfLite model
+        try {
+            tfLite = new Interpreter(loadModelFile(DetectorActivity.this));
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
     }
 
     //load the tfLite model
@@ -82,6 +92,31 @@ public class DetectorActivity extends AppCompatActivity {
         long startOffset = fileDescriptor.getStartOffset();
         long declareLength = fileDescriptor.getDeclaredLength();
         return fileChannel.map(FileChannel.MapMode.READ_ONLY, startOffset, declareLength);
+    }
+
+    //load the image and do the image processing
+    private TensorImage loadImage(final Bitmap bitmap){
+        //load bitmap into a TensorImage
+        inputImageBuffer.load(bitmap);
+
+        //create processor for the tensorflow
+        int cropsize = Math.min(bitmap.getWidth(),bitmap.getHeight());
+
+        ImageProcessor imageProcessor =
+                new ImageProcessor.Builder()
+                        .add(new ResizeWithCropOrPadOp(cropsize, cropsize))
+                        .add(new ResizeOp(x,y, ResizeOp.ResizeMethod.NEAREST_NEIGHBOR))
+                        .add(getPreProcessorNormalizeOP())
+                        .build();
+        return imageProcessor.process(inputImageBuffer);
+    }
+
+    //normalize the image
+    private TensorOperator getPreProcessorNormalizeOP() {
+        return new NormalizeOp(IMAGE_MEAN, IMAGE_STD);
+    }
+    private TensorOperator getPostProcessorNormalizeOP() {
+        return new NormalizeOp(PROBABILITY_MEAN, PROBABILITY_STD);
     }
 
 
